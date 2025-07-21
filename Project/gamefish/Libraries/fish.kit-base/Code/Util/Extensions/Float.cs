be7a009ff96de4e175e4ffc58d2bbf575dd44b3e
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2025-07-21 22:57:15 +0300

    Add an extension for MathX.Clamp that accepts FloatRange as min-max values

--- Project/gamefish/Libraries/fish.kit-base/Code/Util/Extensions/Float.cs
+++ Project/gamefish/Libraries/fish.kit-base/Code/Util/Extensions/Float.cs
@@ -15,4 +15,10 @@ public static partial class GameFish
 	/// </summary>
 	public static int Sign( this float n )
 		=> MathF.Sign( n );
+
+	/// <summary>
+	/// <see cref="MathX.Clamp"/>
+	/// </summary>
+	public static float Clamp( this float n, FloatRange range )
+		=> n.Clamp( range.Min, range.Max );
 }
