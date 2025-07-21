3f36885c072831440e551711b009adf495ded771
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-07-21 11:50:10 -0400

    Safer spec mode cycling, remove exceptions

--- Project/gamefish/Libraries/fish.kit-base/Code/Systems/Agency/Pawns/Player/SpectatorPawn.cs
+++ Project/gamefish/Libraries/fish.kit-base/Code/Systems/Agency/Pawns/Player/SpectatorPawn.cs
@@ -7,7 +7,7 @@ namespace GameFish;
 /// </summary>
 public partial class SpectatorPawn : ControllerPawn
 {
-	public const string GROUP_SPECTATOR = "👻 Spectator";
+	protected const string FEATURE_SPECTATOR = "👻 Spectator";
 
 	public enum SpectatorModes
 	{
@@ -25,26 +25,22 @@ public partial class SpectatorPawn : ControllerPawn
 		[Icon( "videocam" )] ThirdPerson
 	}
 
-	/// <summary>
-	/// TODO: a hack
-	/// </summary>
-	private const int SPECTATOR_MODES_LENGTH = (int)SpectatorModes.ThirdPerson + 1;
-
+	[Sync]
 	[Property]
-	[Feature( GROUP_SPECTATOR )]
+	[Feature( FEATURE_SPECTATOR )]
 	public PlayerPawn Target { get; set; }
 
+	[Sync]
 	[Property]
-	[Feature( GROUP_SPECTATOR )]
+	[Feature( FEATURE_SPECTATOR )]
 	public SpectatorModes Mode { get; set; }
 
 	/// <summary>
 	/// Requirements for a pawn to be observable.
-	///
 	/// For example, you can let the players spectate only their teammates.
 	/// </summary>
 	[Property]
-	[Feature( GROUP_SPECTATOR )]
+	[Feature( FEATURE_SPECTATOR )]
 	public Func<BasePlayer, bool> Filter { get; set; }
 
 	private bool IsAnythingEnabled => HasFirstPersonMode || HasThirdPersonMode || HasFreeRoamMode;
@@ -56,61 +52,61 @@ public partial class SpectatorPawn : ControllerPawn
 		SpectatorModes.FreeRoam => HasFreeRoamMode,
 		_ => throw new NotImplementedException( $"Cannot find a flag for mode {mode}" )
 	};
-	
+
+	public List<SpectatorModes> GetAllowedModes()
+		=> Enum.GetValues<SpectatorModes>()
+			.Where( IsModeEnabled )
+			.ToList();
+
 	private bool InEditor => Scene?.IsEditor ?? true;
+
+	public void CycleMode( int dir )
+	{
+		var modes = GetAllowedModes();
+
+		if ( modes.Count <= 0 )
+			return;
+
+		var iMode = modes.IndexOf( Mode );
+		var iNext = (iMode + dir) % modes.Count;
+
+		Mode = modes.ElementAtOrDefault( iNext );
+	}
 
 	/// <summary>
 	/// Choose the next mode available.
 	/// </summary>
 	[Button( "Next Mode" )]
-	[Feature( GROUP_SPECTATOR ), Group( "Debug" )]
-	[HideIf(nameof(InEditor), true)]
-	public void NextMode()
-	{
-		if ( !IsAnythingEnabled ) return;
-
-		do
-		{
-			Mode = (SpectatorModes)(((int)Mode + 1) % SPECTATOR_MODES_LENGTH);
-		} while ( !IsModeEnabled( Mode ) );
-	}
+	[Feature( FEATURE_SPECTATOR ), Group( "Debug" )]
+	public void NextMode() => CycleMode( 1 );
 
 	/// <summary>
 	/// Choose the previous mode available.
 	/// </summary>
 	[Button( "Previous Mode" )]
-	[Feature( GROUP_SPECTATOR ), Group( "Debug" )]
-	[HideIf(nameof(InEditor), true)]
-	public void PreviousMode()
-	{
-		if ( !IsAnythingEnabled ) return;
-
-		do
-		{
-			Mode = (SpectatorModes)((SPECTATOR_MODES_LENGTH + (int)Mode - 1) % SPECTATOR_MODES_LENGTH);
-		} while ( !IsModeEnabled( Mode ) );
-	}
+	[Feature( FEATURE_SPECTATOR ), Group( "Debug" )]
+	public void PreviousMode() => CycleMode( -1 );
 
 	/// <summary>
 	/// Choose the next mode available.
 	/// </summary>
 	[Button( "Next Target" )]
-	[Feature( GROUP_SPECTATOR ), Group( "Debug" )]
-	[HideIf(nameof(InEditor), true)]
+	[Feature( FEATURE_SPECTATOR ), Group( "Debug" )]
+	[HideIf( nameof( InEditor ), true )]
 	public void NextTarget()
 	{
-		Log.Error( "TODO:" );
+		this.Warn( "Targeting is not yet implemented." );
 	}
 
 	/// <summary>
 	/// Choose the previous mode available.
 	/// </summary>
 	[Button( "Previous Target" )]
-	[Feature( GROUP_SPECTATOR ), Group( "Debug" )]
-	[HideIf(nameof(InEditor), true)]
+	[Feature( FEATURE_SPECTATOR ), Group( "Debug" )]
+	[HideIf( nameof( InEditor ), true )]
 	public void PreviousTarget()
 	{
-		Log.Error( "TODO:" );
+		this.Warn( "Targeting is not yet implemented." );
 	}
 
 	protected override void OnUpdate()
@@ -122,9 +118,6 @@ public partial class SpectatorPawn : ControllerPawn
 			case SpectatorModes.FirstPerson:
 				FirstPersonUpdate();
 				break;
-			
-			default:
-				throw new NotImplementedException( $"Mode {Mode} is not implemented" );
 		}
 	}
 
@@ -134,13 +127,11 @@ public partial class SpectatorPawn : ControllerPawn
 
 		switch ( Mode )
 		{
+			case SpectatorModes.FreeRoam:
 			case SpectatorModes.FirstPerson:
 			case SpectatorModes.ThirdPerson:
 				// rndtrash: There is no need for a FixedUpdate here... not yet
 				break;
-
-			default:
-				throw new NotImplementedException( $"Mode {Mode} is not implemented" );
 		}
 	}
 }
