e2688020328a92442805f6eedc3c57e606bd8d86
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-07-22 16:40:16 -0400

    Add Util/Transforms/Offset struct and extensions

---
+++ Project/gamefish/Libraries/fish.kit-base/Code/Util/Transforms/Offset.cs
@@ -0,0 +1,51 @@
+namespace GameFish;
+
+/// <summary>
+/// A position and a rotation, but not scale. <br />
+/// Makes it very easy to animate
+/// </summary>
+public partial struct Offset
+{
+	public Vector3 Position { readonly get => _pos; set { _pos = value; } }
+	[Hide] private Vector3 _pos;
+
+	public Rotation Rotation { readonly get => _r; set { _r = value; } }
+	[Hide] private Rotation _r = Rotation.Identity;
+
+	public static Vector3 Scale => Vector3.One;
+
+	[Hide]
+	public Transform Transform
+	{
+		readonly get => new( Position, Rotation, Scale );
+		set
+		{
+			Position = value.Position;
+			Rotation = value.Rotation;
+		}
+	}
+
+	public Offset()
+	{
+	}
+
+	public Offset( in Transform t )
+	{
+		Position = t.Position;
+		Rotation = t.Rotation;
+	}
+
+	public Offset( in Vector3 pos, in Rotation r )
+	{
+		Position = pos;
+		Rotation = r;
+	}
+
+	public readonly Offset LerpTo( in Offset offset, in float frac )
+		=> new( Transform.LerpTo( offset.Transform, frac ) );
+
+	/// <param name="t"> The transform to offset from. </param>
+	/// <returns> The resulting transform of this offset from the specified transform. </returns>
+	public readonly Transform ToWorld( in Transform t )
+		=> t.ToWorld( Transform );
+}
---
+++ Project/gamefish/Libraries/fish.kit-base/Code/Util/Transforms/OffsetExtensions.cs
@@ -0,0 +1,60 @@
+namespace GameFish;
+
+public static class OffsetExtensions
+{
+	/// <summary>
+	/// Lerps a transform(probably local!) to an offset's position/rotation while preserving its original scale.
+	/// </summary>
+	public static Transform LerpTo( this in Transform t, in Offset offset, in float frac )
+		=> t.LerpTo( offset.Transform.WithScale( t.Scale ), frac );
+
+	/// <summary>
+	/// Sets a transform(probably local!) to an offset's position/rotation while preserving its original scale.
+	/// </summary>
+	public static Transform SetOffset( this in Transform t, in Offset offset )
+		=> offset.Transform.WithScale( t.Scale );
+
+	/// <summary>
+	/// Sets an object's offset from the specified transform.
+	/// </summary>
+	public static void SetOffset( this GameObject self, in Transform t, in Offset offset )
+	{
+		if ( self.IsValid() )
+			self.WorldTransform = t.ToWorld( offset.Transform ).WithScale( self.WorldScale );
+	}
+
+	/// <summary>
+	/// Sets an object's offset from the specified transform.
+	/// </summary>
+	public static void SetOffset( this Component self, in Transform t, in Offset offset )
+		=> self?.GameObject?.SetOffset( t, offset );
+
+	/// <summary>
+	/// Sets an object's offset relative to another object.
+	/// </summary>
+	public static void SetOffset( this GameObject self, GameObject other, in Offset offset )
+	{
+		if ( self.IsValid() && other.IsValid() )
+			self.SetOffset( other.WorldTransform, offset );
+	}
+
+	/// <summary>
+	/// Sets an object's offset relative to another object.
+	/// </summary>
+	public static void SetOffset( this GameObject self, Component other, in Offset offset )
+	{
+		self?.SetOffset( other?.GameObject, offset );
+	}
+
+	/// <summary>
+	/// Sets an object's offset relative to another object.
+	/// </summary>
+	public static void SetOffset( this Component self, GameObject other, in Offset offset )
+		=> self?.GameObject?.SetOffset( other, offset );
+
+	/// <summary>
+	/// Sets an object's offset relative to another object.
+	/// </summary>
+	public static void SetOffset( this Component self, Component other, in Offset offset )
+		=> self?.GameObject?.SetOffset( other?.GameObject, offset );
+}
