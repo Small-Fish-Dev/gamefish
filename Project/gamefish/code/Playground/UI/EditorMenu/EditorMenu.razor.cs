a72c745d4f87889a2f43c7acb2ce81fc957ebf76
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-12-19 20:55:16 -0500

    Just don't render any editor stuff if there's none

--- Project/gamefish/code/Playground/UI/EditorMenu/EditorMenu.razor.cs
+++ Project/gamefish/code/Playground/UI/EditorMenu/EditorMenu.razor.cs
@@ -6,6 +6,7 @@ namespace Playground.Razor;
 partial class EditorMenu
 {
 	protected static Editor Editor => Editor.Instance;
+	protected static bool HasEditor => Editor.IsValid();
 
 	public static bool IsOpen => Editor.TryGetInstance( out var e ) && e.IsOpen;
 
