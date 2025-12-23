22a22fd365b33be01d9b91c8d4693a743758d470
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-12-23 12:16:50 -0500

    Add/implement standard physics joint base tool

---
+++ Project/gamefish/code/Playground/Editing/Tools/Base/Joint/JointTool.cs
@@ -0,0 +1,197 @@
+namespace Playground;
+
+public abstract class JointTool : EditorTool
+{
+	[Property]
+	[Title( "Joint" )]
+	[Feature( EDITOR ), Group( PREFABS ), Order( PREFABS_ORDER )]
+	public PrefabFile JointPrefab { get; set; }
+
+	[Property]
+	[Title( "Attach" )]
+	[Feature( EDITOR ), Group( SOUNDS ), Order( SOUNDS_ORDER )]
+	public virtual SoundEvent AttachingSound { get; set; }
+
+	public ToolAttachPoint PointTarget { get; set; }
+
+	public ToolAttachPoint Point1 { get; set; }
+	public ToolAttachPoint Point2 { get; set; }
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+
+		if ( !IsSelected )
+			return;
+
+		DrawPointGizmo( Point1 );
+		DrawPointGizmo( Point2 );
+
+		DrawPointGizmo( PointTarget );
+	}
+
+	public override void FrameSimulate( in float deltaTime )
+	{
+		if ( !IsClientAllowed( Client.Local ) )
+			return;
+
+		if ( !TryTrace( out var tr ) )
+			return;
+
+		if ( !tr.Hit || !tr.GameObject.IsValid() )
+			return;
+
+		if ( PressedReload )
+		{
+			if ( !TryClear( tr.GameObject ) )
+				ClearPoints();
+		}
+
+		PointTarget = new( tr );
+	}
+
+	public override bool TryLeftClick()
+		=> TryAddPoint( PointTarget );
+
+	protected virtual bool TryAddPoint( in ToolAttachPoint point )
+	{
+		if ( !point.IsValid() )
+			return false;
+
+		if ( Point1.IsValid() )
+		{
+			Point2 = point;
+			TryAttach( Point1, Point2 );
+
+			return true;
+		}
+
+		Point1 = point;
+
+		return true;
+	}
+
+	protected virtual void ClearPoints()
+	{
+		Point1 = default;
+		Point2 = default;
+	}
+
+	protected abstract void RpcRemoveJoints( GameObject obj );
+
+	/// <summary>
+	/// Lets you tell the tool what type of joint it should clear.
+	/// </summary>
+	/// <returns> If any of its joint could be removed. </returns>
+	public abstract bool TryClear( GameObject obj );
+
+	protected virtual bool TryClear<TJoint>( GameObject obj )
+		where TJoint : JointEntity
+	{
+		if ( !obj.IsValid() )
+			return false;
+
+		var joints = obj.Components.GetAll<TJoint>( FindMode.EverythingInSelfAndDescendants );
+
+		if ( !joints.Any( th => th.IsValid() ) )
+			return false;
+
+		RpcRemoveJoints( obj );
+		return true;
+	}
+
+	public virtual bool ValidTarget( Client cl, in SceneTraceResult tr )
+	{
+		if ( !tr.Hit || !tr.GameObject.IsValid() )
+			return false;
+
+		if ( Pawn.TryGet( tr.GameObject, out _ ) )
+			return false;
+
+		return true;
+	}
+
+	public virtual bool ValidAttachment( in ToolAttachPoint point )
+	{
+		if ( Pawn.TryGet( point.Object, out _ ) )
+			return false;
+
+		return true;
+	}
+
+	public abstract bool TryAttach( in ToolAttachPoint point1, in ToolAttachPoint point2 );
+
+	protected virtual bool TryAttach<TJoint>( in ToolAttachPoint point1, in ToolAttachPoint point2 )
+		where TJoint : JointEntity
+	{
+		if ( !IsClientAllowed( Client.Local ) )
+			return false;
+
+		if ( !point1.IsValid() || !ValidAttachment( point1 ) )
+			return false;
+
+		if ( !point2.IsValid() || !ValidAttachment( point2 ) )
+			return false;
+
+		if ( !JointPrefab.TrySpawn( out var jointObj ) )
+		{
+			this.Warn( $"Couldn't find/spawn {typeof( TJoint )} prefab:[{JointPrefab}]!" );
+			return false;
+		}
+
+		jointObj.NetworkInterpolation = false;
+
+		if ( !jointObj.Components.TryGet<TJoint>( out var joint ) )
+		{
+			this.Warn( $"No {typeof( TJoint )} on obj:[{jointObj}]!" );
+			jointObj.Destroy();
+			return false;
+		}
+
+		joint.ParentPoint = point1;
+		joint.TargetPoint = point2;
+
+		ApplySettings( joint );
+
+		joint.TrySetNetworkOwner( Connection.Local, allowProxy: true );
+
+		if ( !joint.TryAttachTo( point1, point2 ) )
+		{
+			this.Warn( $"Couldn't attach joint:[{joint}]!" );
+			jointObj.Destroy();
+			return false;
+		}
+
+		ClearPoints();
+
+		return true;
+	}
+
+	public virtual void ApplySettings<TJoint>( TJoint joint )
+		where TJoint : JointEntity
+	{
+	}
+
+	protected virtual void DrawPointGizmo( in ToolAttachPoint point )
+	{
+		if ( !point.Object.IsValid() || !point.Offset.HasValue )
+			return;
+
+		if ( !ValidAttachment( point ) )
+			return;
+
+		var c = Color.Green.WithAlpha( 0.3f );
+
+		var tObj = point.Object.WorldTransform;
+		var tArrow = tObj.ToWorld( point.Offset.Value );
+
+		var dir = point.HitNormal ?? tArrow.Forward;
+
+		this.DrawArrow(
+			from: tArrow.Position,
+			to: tArrow.Position + (dir * 6f),
+			c: c, len: 0.1f, w: 5f, th: 4f,
+			tWorld: global::Transform.Zero
+		);
+	}
+}
--- Project/gamefish/code/Playground/Editing/Tools/Mechanical/Axle/ArmTool.cs
+++ Project/gamefish/code/Playground/Editing/Tools/Mechanical/Axle/ArmTool.cs
@@ -1,17 +1,7 @@
 namespace Playground;
 
-public partial class ArmTool : EditorTool
+public partial class ArmTool : JointTool
 {
-	[Property]
-	[Title( "Arm" )]
-	[Feature( EDITOR ), Group( PREFABS ), Order( PREFABS_ORDER )]
-	public PrefabFile ArmPrefab { get; set; }
-
-	[Property]
-	[Title( "Attach" )]
-	[Feature( EDITOR ), Group( SOUNDS ), Order( SOUNDS_ORDER )]
-	public virtual SoundEvent AttachingSound { get; set; }
-
 	/// <summary>
 	/// The key you press to activate the thruster you're placing.
 	/// </summary>
@@ -19,80 +9,10 @@ public partial class ArmTool : EditorTool
 	[Feature( EDITOR ), Group( SETTINGS ), Order( SETTINGS_ORDER )]
 	public virtual ArmSettings ArmSettings { get; set; }
 
-	public bool HasFirstPoint { get; set; }
+	public override bool TryAttach( in ToolAttachPoint point1, in ToolAttachPoint point2 )
+		=> TryAttach<Arm>( in point1, in point2 );
 
-	public ToolAttachPoint Point1 { get; set; }
-	public ToolAttachPoint Point2 { get; set; }
-
-	protected virtual void ClearPoints()
-	{
-		HasFirstPoint = false;
-
-		Point1 = default;
-		Point2 = default;
-	}
-
-	public override bool TryLeftClick()
-	{
-		if ( HasFirstPoint && Point1.IsValid() && Point2.IsValid() )
-		{
-			TryAttachArm();
-			return true;
-		}
-
-		if ( Point1.IsValid() )
-		{
-			TrySetStartPoint( Point1 );
-			return true;
-		}
-
-		return true;
-	}
-
-	public override void FrameSimulate( in float deltaTime )
-	{
-		if ( !IsClientAllowed( Client.Local ) )
-			return;
-
-		if ( !TryTrace( out var tr ) )
-			return;
-
-		if ( !tr.Hit || !tr.GameObject.IsValid() )
-			return;
-
-		// Clear Arms
-		if ( PressedReload )
-		{
-			TryClearArms( tr.GameObject );
-			ClearPoints();
-
-			return;
-		}
-
-		if ( !Point1.IsValid() )
-			HasFirstPoint = false;
-
-		if ( !HasFirstPoint )
-		{
-			// First Point
-			if ( ValidTarget( Client.Local, in tr ) )
-				Point1 = new( tr );
-		}
-		else
-		{
-			// Second Point
-			if ( ValidTarget( Client.Local, in tr ) )
-				Point2 = new( tr );
-		}
-
-		if ( Point1.IsValid() )
-			DrawArmGizmo( Point1 );
-
-		if ( Point2.IsValid() )
-			DrawArmGizmo( Point2 );
-	}
-
-	protected virtual void DrawArmGizmo( in ToolAttachPoint point )
+	protected override void DrawPointGizmo( in ToolAttachPoint point )
 	{
 		if ( !point.Object.IsValid() || !point.Offset.HasValue )
 			return;
@@ -115,102 +35,19 @@ public partial class ArmTool : EditorTool
 		);
 	}
 
-	protected virtual bool TrySetStartPoint( in ToolAttachPoint point )
+	public override void ApplySettings<TJoint>( TJoint joint )
 	{
-		if ( !point.IsValid() )
-		{
-			Point1 = default;
-			HasFirstPoint = false;
-
-			return false;
-		}
-
-		Point1 = point;
-		HasFirstPoint = true;
-
-		return true;
-	}
-
-	protected virtual bool TryAttachArm()
-	{
-		if ( !IsClientAllowed( Client.Local ) )
-			return false;
-
-		if ( !Point1.IsValid() || !ValidAttachment( Point1 ) )
-			return false;
-
-		if ( !Point2.IsValid() || !ValidAttachment( Point2 ) )
-			return false;
-
-		if ( !ArmPrefab.TrySpawn( out var armObj ) )
-		{
-			this.Warn( $"Couldn't find/spawn {typeof( Arm )} prefab:[{ArmPrefab}]!" );
-			return false;
-		}
-
-		armObj.NetworkInterpolation = false;
-
-		if ( !armObj.Components.TryGet<Arm>( out var arm ) )
-		{
-			this.Warn( $"No {typeof( Arm )} on obj:[{armObj}]!" );
-			armObj.Destroy();
-			return false;
-		}
-
-		arm.ParentPoint = Point1;
-		arm.TargetPoint = Point2;
+		if ( joint is not Arm arm )
+			return;
 
 		arm.Settings = ArmSettings;
-
-		arm.TrySetNetworkOwner( Connection.Local, allowProxy: true );
-
-		if ( !arm.TryAttachTo( Point1, Point2 ) )
-		{
-			this.Warn( $"Couldn't attach arm:[{arm}]!" );
-			armObj.Destroy();
-			return false;
-		}
-
-		ClearPoints();
-
-		return true;
 	}
 
-	protected virtual bool TryClearArms( GameObject obj )
-	{
-		if ( !obj.IsValid() )
-			return false;
-
-		var arms = obj.Components.GetAll<Arm>( FindMode.EverythingInSelfAndDescendants );
-
-		if ( !arms.Any( th => th.IsValid() ) )
-			return false;
-
-		RpcRemoveArms( obj );
-		return true;
-	}
-
-	public virtual bool ValidTarget( Client cl, in SceneTraceResult tr )
-	{
-		if ( !tr.Hit || !tr.GameObject.IsValid() )
-			return false;
-
-		if ( Pawn.TryGet( tr.GameObject, out _ ) )
-			return false;
-
-		return true;
-	}
-
-	public virtual bool ValidAttachment( in ToolAttachPoint point )
-	{
-		if ( Pawn.TryGet( point.Object, out _ ) )
-			return false;
-
-		return true;
-	}
+	public override bool TryClear( GameObject obj )
+		=> TryClear<Arm>( obj );
 
 	[Rpc.Host]
-	protected void RpcRemoveArms( GameObject obj )
+	protected override void RpcRemoveJoints( GameObject obj )
 	{
 		if ( !obj.IsValid() || !TryUse( Rpc.Caller, out _ ) )
 			return;
