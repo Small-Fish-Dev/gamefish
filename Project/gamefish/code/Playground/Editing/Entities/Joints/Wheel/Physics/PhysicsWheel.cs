a1d025b442b0e69761629a1d7eaf01d38505bb7d
yart <100040020+youarereadingthis@users.noreply.github.com>
2025-12-24 20:51:50 -0500

    Add Physics Wheel tool (WIP, almost done)

---
+++ Project/gamefish/code/Playground/Editing/Entities/Joints/Wheel/Physics/PhysicsWheel.cs
@@ -0,0 +1,173 @@
+namespace Playground;
+
+[Icon( "precision_manufacturing" )]
+public partial class PhysicsWheel : EditorEntity
+{
+	protected const int PHYSICS_ORDER = EDITOR_ORDER + 10;
+
+	[Property]
+	[Feature( EDITOR ), Group( PHYSICS ), Order( PHYSICS_ORDER )]
+	public Sandbox.WheelJoint Joint { get; set; }
+
+	[Sync]
+	[Property, InlineEditor]
+	[Feature( EDITOR ), Group( PHYSICS ), Order( PHYSICS_ORDER )]
+	public PhysicsWheelSettings Settings { get; set; }
+
+	[Sync]
+	[Property, ReadOnly]
+	[Feature( EDITOR ), Group( PHYSICS ), Order( PHYSICS_ORDER )]
+	public Vector2 DriveInput { get; set; } = Vector2.Zero;
+
+	[Sync]
+	public ToolAttachPoint ParentPoint { get; set; }
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+
+		// Snap to the offset without lag if we're the owner.
+		if ( GameObject.Parent.IsValid() && !GameObject.Parent.IsProxy )
+			TryAttachTo( point: ParentPoint );
+	}
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+
+		UpdateInput( Time.Delta );
+	}
+
+	protected override void OnDestroy()
+	{
+		base.OnDestroy();
+
+		if ( !this.InGame() || !GameObject.IsValid() )
+			return;
+
+		// Auto-cleanup empty objects that only had a spiner.
+		var comps = GameObject.Components.GetAll( FindMode.EverythingInSelf )
+			.Where( comp => comp.IsValid() );
+
+		if ( !comps.Any() )
+			GameObject.Destroy();
+	}
+
+	protected void UpdateInput( in float deltaTime )
+	{
+		if ( IsProxy )
+			return;
+
+		var drive = Vector2.Zero;
+
+		// Pitch
+		var bForward = !Settings.KeyForward.IsBlank()
+			&& Input.Keyboard.Down( Settings.KeyForward );
+
+		var bReverse = !Settings.KeyReverse.IsBlank()
+			&& Input.Keyboard.Down( Settings.KeyReverse );
+
+		if ( bForward )
+			drive.y += 1;
+
+		if ( bReverse )
+			drive.y -= 1;
+
+		var bLeft = !Settings.KeyLeft.IsBlank()
+			&& Input.Keyboard.Down( Settings.KeyLeft );
+
+		var bRight = !Settings.KeyRight.IsBlank()
+			&& Input.Keyboard.Down( Settings.KeyLeft );
+
+		if ( bLeft )
+			drive.x -= 1;
+
+		if ( bRight )
+			drive.x += 1;
+
+		DriveInput = drive;
+	}
+
+	public void UpdateWheel( in float deltaTime )
+	{
+		if ( !Joint.IsValid() )
+			return;
+
+		Joint.TargetSteeringAngle = DriveInput.x * 30f;
+		Joint.SpinMotorSpeed = DriveInput.y * 360f;
+	}
+
+	public bool TryAttachTo( in ToolAttachPoint point )
+	{
+		// Must have a Wheel(the entire point).
+		if ( !Joint.IsValid() )
+			return false;
+
+		var objTarget = point.Object;
+
+		if ( !objTarget.IsValid() )
+			return false;
+
+		// Disgraceful..
+		if ( objTarget == GameObject )
+			return false;
+
+		var aPhys = Physics.FindBody( objTarget );
+
+		if ( !aPhys.IsValid() || point.Offset is not Offset offset )
+			return false;
+
+		var bPhys = Physics.FindBody( GameObject );
+
+		if ( !bPhys.IsValid() )
+			return false;
+
+		var tPoint = objTarget.WorldTransform.WithOffset( offset );
+
+		var aPhysLocal = aPhys.Transform.ToLocal( tPoint );
+		var bPhysLocal = bPhys.Transform.ToLocal( tPoint );
+
+		// var aPhysPoint = new PhysicsPoint( aPhys, aPhysLocal.Position, aPhysLocal.Rotation );
+		// var bPhysPoint = new PhysicsPoint( bPhys, bPhysLocal.Position, bPhysLocal.Rotation );
+
+		if ( !ITransform.IsValid( aPhysLocal ) || !ITransform.IsValid( bPhysLocal ) )
+			return false;
+
+		WorldTransform = tPoint;
+
+		Transform.ClearInterpolation();
+
+		// Let the engine's component handle it.
+		Joint.Attachment = Sandbox.Joint.AttachmentMode.LocalFrames;
+
+		Joint.LocalFrame2 = aPhysLocal;
+		Joint.LocalFrame1 = bPhysLocal;
+
+		// Set the other body.
+		Joint.Body = objTarget;
+
+		return true;
+	}
+
+	protected void DrawWheelGizmos()
+	{
+		var objParent = ParentPoint.Object;
+
+		if ( !objParent.IsValid() )
+			return;
+
+		if ( ParentPoint.Offset is not Offset parentOffset )
+			return;
+
+		var c = Color.White.WithAlpha( 0.3f );
+
+		var tParentPoint = objParent.WorldTransform.WithOffset( parentOffset );
+
+		this.DrawArrow(
+			from: WorldPosition,
+			to: tParentPoint.Position,
+			c: c, len: 7f, w: 2f, th: 4f,
+			tWorld: global::Transform.Zero
+		);
+	}
+}
---
+++ Project/gamefish/code/Playground/Editing/Tools/Vehicle/Wheel/Physics/PhysicsWheelTool.cs
@@ -0,0 +1,96 @@
+namespace Playground;
+
+public partial class PhysicsWheelTool : JointTool
+{
+	[Property, InlineEditor]
+	[Feature( EDITOR ), Group( SETTINGS ), Order( SETTINGS_ORDER )]
+	public virtual PhysicsWheelSettings JointSettings { get; set; }
+
+	public override bool TryAddPointAtTarget()
+		=> TryAttach( PointTarget );
+
+	public override bool TryAttach( in ToolAttachPoint hitPoint, in ToolAttachPoint _ )
+		=> false;
+
+	protected override bool TryAttach<TJoint>( in ToolAttachPoint hitPoint, in ToolAttachPoint _ )
+		=> false;
+
+	protected bool TryAttach( in ToolAttachPoint hitPoint )
+	{
+		if ( !IsClientAllowed( Client.Local ) )
+			return false;
+
+		if ( !hitPoint.IsValid() || !ValidAttachment( hitPoint ) )
+			return false;
+
+		var tHit = hitPoint.Object.WorldTransform.WithOffset( hitPoint.Offset.Value );
+
+		if ( !JointPrefab.TrySpawn( in tHit, out var jointObj ) )
+		{
+			this.Warn( $"Couldn't find/spawn {typeof( PhysicsWheel )} prefab:[{JointPrefab}]!" );
+			return false;
+		}
+
+		jointObj.NetworkInterpolation = false;
+
+		if ( !jointObj.Components.TryGet<PhysicsWheel>( out var joint ) )
+		{
+			this.Warn( $"No {typeof( PhysicsWheel )} on obj:[{jointObj}]!" );
+			jointObj.Destroy();
+			return false;
+		}
+
+		joint.ParentPoint = hitPoint;
+
+		joint.TrySetNetworkOwner( Connection.Local, allowProxy: true );
+
+		if ( !joint.TryAttachTo( hitPoint ) )
+		{
+			this.Warn( $"Couldn't attach joint:[{joint}]!" );
+			jointObj.Destroy();
+			return false;
+		}
+
+		ClearPoints();
+
+		return true;
+	}
+
+	protected override void DrawJointGizmos()
+	{
+	}
+
+	protected override void DrawPointGizmo( in ToolAttachPoint point )
+	{
+		if ( !point.Object.IsValid() || !point.Offset.HasValue )
+			return;
+
+		if ( !ValidAttachment( point ) )
+			return;
+
+		var c = Color.White.Desaturate( 0.4f ).WithAlpha( 0.3f );
+
+		var tObj = point.Object.WorldTransform;
+		var tArrow = tObj.ToWorld( point.Offset.Value );
+
+		var dir = point.HitNormal ?? tArrow.Forward;
+
+		this.DrawArrow(
+			from: tArrow.Position + (dir * 7f),
+			to: tArrow.Position,
+			c: c, len: 7f, w: 2f, th: 3f,
+			tWorld: global::Transform.Zero
+		);
+	}
+
+	public override void ApplySettings<TJoint>( TJoint joint )
+	{
+	}
+
+	public override bool TryClear( GameObject obj )
+		=> false;
+
+	protected override void RpcRemoveJoints( GameObject obj )
+	{
+	}
+}
