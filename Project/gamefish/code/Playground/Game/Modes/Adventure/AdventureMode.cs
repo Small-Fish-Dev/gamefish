15299f3a79bcd73a830cdff7397a6c2b7f513b53
yart <100040020+youarereadingthis@users.noreply.github.com>
2026-01-04 17:11:47 -0500

    Add blank Adventure gamemode 🏕⚔🍖🏡🔧🚙🧪🚀
    fuck you asshole

---
+++ Project/gamefish/code/Playground/Game/Modes/Adventure/AdventureMode.cs
@@ -0,0 +1,10 @@
+using GameFish;
+
+namespace Playground;
+
+[Icon( "explore" )]
+public partial class AdventureMode : Gamemode
+{
+	public override string Name { get; } = "Adventure";
+	public override string Description { get; } = "Explore, fight, grow, cook, unlock, craft and engineer your way to the space age.";
+}
