3b347c44b760610cc31a27a41c2477a3ff0aca17
yart <100040020+youarereadingthis@users.noreply.github.com>
2026-01-06 10:41:29 -0500

    Add Roleplay mode
    lmao

---
+++ Project/gamefish/code/Playground/Game/Modes/Roleplay/RoleplayMode.cs
@@ -0,0 +1,10 @@
+using GameFish;
+
+namespace Fishbox;
+
+[Icon( "theater_comedy" )]
+public partial class RoleplayMode : Gamemode
+{
+	public override string Name { get; } = "Roleplay";
+	public override string Description { get; } = "Hang out and get high, work jobs or fight in the arena.";
+}
