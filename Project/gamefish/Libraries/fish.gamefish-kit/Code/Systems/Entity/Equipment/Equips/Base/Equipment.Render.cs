37ae94c9d8049eba4af400277dcaf867d3c1b1f8
Gio <me@gio.blue>
2026-03-29 00:17:43 +0800

    Allow setting WorldModel in equipment directly

--- Project/gamefish/Libraries/fish.gamefish-kit/Code/Systems/Entity/Equipment/Equips/Base/Equipment.Render.cs
+++ Project/gamefish/Libraries/fish.gamefish-kit/Code/Systems/Entity/Equipment/Equips/Base/Equipment.Render.cs
@@ -10,14 +10,30 @@ partial class Equipment : ISkinned
 
 	[Property]
 	[Feature( EQUIP ), Group( MODELS )]
-	public Model WorldModel { get => WorldRenderer?.Model; set { if ( WorldRenderer.IsValid() ) WorldRenderer.Model = value; } }
+	public Model WorldModel
+	{
+		get =>
+			WorldRenderer?.Model;
+		set
+		{
+			if ( WorldRenderer.IsValid() )
+				WorldRenderer.Model = value;
+			else if ( Application.IsEditor )
+			{
+				WorldRenderer ??= Components.Create<SkinnedModelRenderer>();
+				WorldRenderer.Model = value;
+				_wr = WorldRenderer;
+			}
+		}
+	}
 
 	[Property]
 	[Feature( EQUIP ), Group( MODELS )]
 	public virtual SkinnedModelRenderer WorldRenderer
 	{
 		// Auto-cache the component.
-		get => _wr.IsValid() ? _wr
+		get => _wr.IsValid()
+			? _wr
 			: _wr = Components?.Get<SkinnedModelRenderer>( FindMode.EverythingInDescendants );
 
 		set { _wr = value; }
