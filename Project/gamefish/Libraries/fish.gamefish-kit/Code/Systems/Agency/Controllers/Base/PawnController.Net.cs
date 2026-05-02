f72d8b5ecd3984db3e3a89558534abc5f71db02c
yart <100040020+youarereadingthis@users.noreply.github.com>
2026-05-01 20:22:29 -0400

    Don't prevent networking of `PawnController`

--- Project/gamefish/Libraries/fish.gamefish-kit/Code/Systems/Agency/Controllers/Base/PawnController.Net.cs
+++
@@ -1,9 +0,0 @@
-namespace GameFish;
-
-partial class PawnController
-{
-	protected override bool IsNetworkSetupAllowed() => false;
-	protected override bool? IsNetworkedOverride => false;
-
-	public override void SetupNetworking( bool force = false ) { }
-}
