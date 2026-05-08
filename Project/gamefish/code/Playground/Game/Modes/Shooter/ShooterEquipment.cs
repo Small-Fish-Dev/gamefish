4cd72a17a4bc24d4bf840b12e3ea4a7772d22983
yart <100040020+youarereadingthis@users.noreply.github.com>
2026-05-08 18:46:49 -0400

    `ShooterEquipment` has hardcoded velocity sway

--- Project/gamefish/code/Playground/Game/Modes/Shooter/ShooterEquipment.cs
+++ Project/gamefish/code/Playground/Game/Modes/Shooter/ShooterEquipment.cs
@@ -2,9 +2,29 @@ namespace Fishbox;
 
 public class ShooterEquipment : Equipment
 {
+	public virtual bool IsSighted => Input.Down( "Attack2" );
+
+	public override void UpdateOffset( in float speed, in float deltaTime )
+	{
+		base.UpdateOffset( speed, deltaTime );
+
+		if ( !Pawn.IsValid() )
+			return;
+
+		var sway = IsSighted ? 0.01f : 0.02f;
+
+		var vel = Pawn.EyeRotation.Inverse * Pawn.Velocity;
+		vel = (vel * deltaTime * sway).ClampLength( 10f );
+
+		var bobOffset = Offset;
+		bobOffset.Position -= vel;
+
+		Offset = bobOffset;
+	}
+
 	public override Offset GetViewOffsetTarget()
 	{
-		if ( Input.Down( "Attack2" ) )
+		if ( IsSighted )
 			return AimingOffset;
 
 		return base.GetViewOffsetTarget();
